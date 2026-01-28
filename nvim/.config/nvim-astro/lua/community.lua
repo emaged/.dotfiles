6c9ac52a9093bb266b359f6f82940a3e22ba221e
Emiel de Jongh Swemer <emieldejonghswemer@gmail.com>
2026-01-29 00:21:11 +0100

    nvim sync

--- nvim/.config/nvim-astro/lua/community.lua
+++ nvim/.config/nvim-astro/lua/community.lua
@@ -8,6 +8,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+  { import = "astrocommunity.pack.python" },
   -- import/override with your plugins folder
 }
