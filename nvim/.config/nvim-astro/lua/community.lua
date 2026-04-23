c98291a8124e90ff289033c99c5660d89dd4b43b
Emiel de Jongh Swemer <emieldejonghswemer@gmail.com>
2026-04-23 13:07:24 +0200

    updated nvim-astro

--- nvim/.config/nvim-astro/lua/community.lua
+++ nvim/.config/nvim-astro/lua/community.lua
@@ -1,4 +1,4 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -8,8 +8,5 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- { import = "astrocommunity.pack.python" },
-  -- { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.ai.sidekick-nvim" },
   -- import/override with your plugins folder
 }
