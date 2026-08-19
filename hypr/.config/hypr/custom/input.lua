f0d67996be1d40aeb9076e8cfea60602ff823e70
emaged <emieldejonghswemer@gmail.com>
2026-08-19 12:21:52 +0200

    added flat accel to mouse config

--- hypr/.config/hypr/custom/input.lua
+++ hypr/.config/hypr/custom/input.lua
@@ -1,6 +1,7 @@
 hl.config({
   input = {
     kb_options = "caps:escape,shift:both_capslock_cancel",
+    accel_profile = "flat",
     touchpad = {
       natural_scroll = true,
       clickfinger_behavior = false,
